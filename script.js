be9affb79cfe6d56c9a834dbeb245bff3aaf8402
Capitano781 <el.capitano781@gmail.com>
2026-01-11 23:44:17 +0530

    create page skeleton

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// const ChangeTheme = () => {
+//     const rootTag = document.documentElement;
+//     const toggleTheme = document.querySelector('#toggleTheme');
+
+//     toggleTheme.addEventListener('click', () => {
+//         const currentTheme = rootTag.getAttribute('data-theme');
+//         rootTag.setAttribute('data-theme', currentTheme === 'dark' ? 'light' : 'dark');
+//     });
+// };
+
+// ChangeTheme();
